9d41bf339cc78e1d2d098165b0d94a68f44181bd
Student <student@com>
2026-09-24 11:35:47 +0600

    uupdate

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int hours = (n / 3600) % 24;
+        int minutes = (n / 60) % 60;
+        int seconds = n % 60;
+
+        System.out.printf("%d:%02d:%02d", hours, minutes, seconds);
+        System.out.printf("%d:%02d:%02d", hours, minutes, seconds);
+    }
 }
